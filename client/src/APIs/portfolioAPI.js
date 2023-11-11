1d2d0b9d0d5a0f9c40cef14602d210251e6df78a
Hristo Hristov <hrshristov@gmail.com>
2023-11-11 17:50:52 +0200

    add portfolio component

---
+++ client/src/APIs/portfolioAPI.js
@@ -0,0 +1,48 @@
+const baseUrl = "http://localhost:3030/jsonstore/portfolio";
+
+export const getAll = async () => {
+    const response = await fetch(baseUrl);
+    const result = await response.json();
+
+    const data = Object.values(result);
+
+    return data;
+};
+
+// export const getOne = async (userId) => {
+//     const response = await fetch(`${baseUrl}/${userId}`);
+//     const result = await response.json();
+
+//     return result;
+// };
+
+// export const create = async (data) => {
+//     const body = {
+//         type: data.type,
+//         coinId: data.coinId,
+//         quanity: data.quantity,
+//         pricePer: data.pricePer,
+//         fee: data.fee,
+//         note: data.note,
+//     };
+
+//     const response = await fetch(baseUrl, {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(body),
+//     });
+
+//     const result = await response.json();
+//     console.log(result);
+//     return result;
+// };
+
+// export const remove = async (entryId) => {
+//     const response = await fetch(`${baseUrl}/${entryId}`, { method: "DELETE" });
+
+//     const result = await response.json();
+
+//     return result;
+// };
---
+++ client/src/components/PortfolioList.jsx
@@ -0,0 +1,58 @@
+import * as portfolioAPI from "../APIs/portfolioAPI.js";
+import { formatNumber, formatPrice } from "../utils/formatUtils.js";
+import { useState, useEffect } from "react";
+import plus from "../assets/plus.png";
+import bin from "../assets/bin.png";
+import info from "../assets/info.png";
+import Delete from "./Delete.jsx";
+
+export default function PortfolioList() {
+    const [entries, setEntries] = useState([]);
+
+    useEffect(() => {
+        portfolioAPI
+            .getAll()
+            .then((result) => setEntries(result))
+            .catch((err) => console.log(err));
+    }, []);
+    return (
+        <>
+            {entries.map((entry) => (
+                <>
+                    <div className="divider"></div>
+                    <div className="coin-row" key={entry.uuid}>
+                        <div className="d-flex gap-2 coin-icon-conteiner">
+                            <div className="coin-icon">
+                                <img className="img" src={entry.iconUrl} />
+                            </div>
+                            <div className="d-flex-center-content gap-2">
+                                <span>{entry.name}</span>
+                                <span>{entry.symbol}</span>
+                            </div>
+                        </div>
+                        <div>${formatPrice(entry.price.toString())}</div>
+                        <div className="up">+3%</div>
+                        <div className="d-flex-column gap-1">
+                            <div>$1359.24</div>
+                            <div>1 BTC</div>
+                        </div>
+                        <div>${formatPrice("23215.42")}</div>
+                        <div>12</div>
+                        <div className="d-flex-end-content gap-2">
+                            <button className="button-action">
+                                <img src={info} width="16" height="16" />
+                            </button>
+                            <button className="button-action">
+                                <img src={plus} width="16" height="16" />
+                            </button>
+                            <button className="button-action">
+                                <img src={bin} width="18" height="18" />
+                            </button>
+                        </div>
+                    </div>
+                </>
+            ))}
+            <Delete />
+        </>
+    );
+}
