51da99440fe8ad47f9e48e7bbe07119781e8b7e3
Hristo Hristov <hrshristov@gmail.com>
2023-11-12 21:59:51 +0200

    add footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <>
+            <footer className="d-flex-center-content">
+                <p>All rights reserver &copy; </p>
+            </footer>
+        </>
+    );
+}
