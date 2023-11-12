acc8f2b8dce18a08733afa4245e3618759051a1f
Hristo Hristov <hrshristov@gmail.com>
2023-11-12 17:34:46 +0200

    fix a big

--- client/src/components/MainSectionHeader.jsx
+++ client/src/components/MainSectionHeader.jsx
@@ -19,15 +19,27 @@ export default function TableHeader({ title }) {
             "Profit/Loss",
             "Actions",
         ];
+
+        return (
+            <div className="coins coin-row coins-content-head">
+                <div className="d-flex gap-2 coin-name-conteiner justify-content-start">
+                    {titles[0]}
+                </div>
+                <div>{titles[1]}</div>
+                <div>{titles[2]}</div>
+                <div>{titles[3]}</div>
+                <div>{titles[4]}</div>
+                <div>{titles[5]}</div>
+                <div>{titles[6]}</div>
+            </div>
+        );
     }
+
     return (
         <div className="coin-row coins-content-head">
             <div className="rank">{titles[0]}</div>
             <div className="d-flex gap-2 coin-icon-conteiner justify-content-start">
-                <div className="coin-icon"></div>
-                <div className="d-flex-center-content gap-2">
-                    <span>{titles[1]}</span>
-                </div>
+                {titles[1]}
             </div>
             <div>{titles[2]}</div>
             <div>{titles[3]}</div>
