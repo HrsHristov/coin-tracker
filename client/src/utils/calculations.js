f565458b9ddf73037e4ca0e45807c72c095fdb10
Hristo Hristov <hrshristov@gmail.com>
2023-11-18 22:38:56 +0200

    Add calculations in utils

--- client/src/utils/calculations.js
+++ client/src/utils/calculations.js
@@ -1,1 +1,20 @@
-import * as portfolioAPI from "../APIs/portfolioAPI.js";
+import { formatPrice } from "./formatUtils.js";
+
+export const calculateHoldings = (price, quanity) => {
+    const holdings = formatPrice((price * quanity).toString());
+
+    return holdings;
+};
+
+export const calculatePNL = (price, buyPrice, quanity) => {
+    const priceDifferce = price - buyPrice;
+    const pnl = priceDifferce * quanity;
+
+    return formatPrice(pnl.toString());
+};
+
+export const calculatePNLPercentage = (price, buyPrice, quanity) => {
+    const pnl = 100 * (price / buyPrice) - 100;
+
+    return formatPrice(pnl.toString());
+};
