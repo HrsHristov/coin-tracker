7f30e76ae19955f3c92e36856ceadc0377c7999c
Hristo Hristov <hrshristov@gmail.com>
2023-11-19 17:25:30 +0200

    Revert

--- client/src/components/PortfolioList/PortfolioList.jsx
+++ client/src/components/PortfolioList/PortfolioList.jsx
@@ -4,11 +4,15 @@ import { useState, useEffect } from "react";
 import ListWrapper from "../ListWrapper/ListWrapper.jsx";
 import ListHeader from "../ListHeader/ListHeader.jsx";
 import PortfolioListRow from "../PortfolioListRow/PortfolioListRow.jsx";
+import DeleteModal from "../DeleteModal/DeleteModal.jsx";
+import CreateEntryModal from "../CreateEntryModal/CreateEntryModal.jsx";
+import EntryInfoModal from "../EntryInfoModal/EntryInfoModal.jsx";
 
 export default function PortfolioList() {
     const [entries, setEntries] = useState([]);
-
-    console.log(entries);
+    const [selectedEntry, setSelectedEntry] = useState(null);
+    const [showDelete, setShowDelete] = useState(false);
+    const [showInfo, setShowInfo] = useState(false);
 
     useEffect(() => {
         portfolioAPI
@@ -17,13 +21,57 @@ export default function PortfolioList() {
             .catch((err) => console.log(err));
     }, []);
 
-    return (
-        <ListWrapper>
-            <ListHeader title="portfolio" />
+    //Info Handler
+    const entryInfoClickHandler = async (entryId) => {
+        setSelectedEntry(entryId);
+        setShowInfo(true);
+    };
 
-            {entries.map((entry) => (
-                <PortfolioListRow key={entry.uuid} {...entry} />
-            ))}
-        </ListWrapper>
+    //Delete Handler
+    const deleteEntryClickHandler = async (entryId) => {
+        setSelectedEntry(entryId);
+        setShowDelete(true);
+    };
+
+    const deleteEntryHandler = async () => {
+        const result = await portfolioAPI.remove(selectedEntry);
+        setEntries((state) =>
+            state.filter((entry) => entry._id !== selectedEntry)
+        );
+        setShowDelete(false);
+    };
+
+    return (
+        <>
+            {showDelete && (
+                <DeleteModal
+                    onClose={() => setShowDelete(false)}
+                    onDelete={deleteEntryHandler}
+                />
+            )}
+
+            {showInfo && (
+                <EntryInfoModal
+                    onClose={() => setShowInfo(false)}
+                    entryId={selectedEntry}
+                />
+            )}
+
+            <button type="submit" className="primary-button px-5">
+                Add Entry
+            </button>
+            <ListWrapper>
+                <ListHeader title="portfolio" />
+
+                {entries.map((entry) => (
+                    <PortfolioListRow
+                        key={entry.uuid}
+                        {...entry}
+                        onInfoClick={entryInfoClickHandler}
+                        onDeleteClick={deleteEntryClickHandler}
+                    />
+                ))}
+            </ListWrapper>
+        </>
     );
 }
