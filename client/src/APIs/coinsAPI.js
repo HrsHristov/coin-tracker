0fa1a6b75b5c312a20663a28a5ebe4344e25d39c
Hristo Hristov <hrshristov@gmail.com>
2023-11-19 17:24:12 +0200

    Add more Coin API calls

--- client/src/APIs/coinsAPI.js
+++ client/src/APIs/coinsAPI.js
@@ -16,3 +16,22 @@ export const getAll = async () => {
 
     return data;
 };
+
+export const getCoinInfo = async (uuid) => {
+    const url = `https://coinranking1.p.rapidapi.com/coin/${uuid}/`;
+    const options = {
+        method: "GET",
+        headers: {
+            "X-RapidAPI-Key":
+                "bc00d31b19mshd31ac5eb9c9811ap1bcc72jsn4e9b00fd6833",
+            "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+        },
+    };
+
+    const response = await fetch(url, options);
+    const result = await response.json();
+
+    // console.log(result);
+
+    return result;
+};
