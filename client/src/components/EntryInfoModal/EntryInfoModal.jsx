ca11d0079471c7ad18024206c9c50fcad388288d
Hristo Hristov <hrshristov@gmail.com>
2023-11-19 17:24:44 +0200

    Make Entry Info modal

--- client/src/components/EntryInfoModal/EntryInfoModal.jsx
+++ client/src/components/EntryInfoModal/EntryInfoModal.jsx
@@ -1,12 +1,31 @@
-import cross from "../assets/cross.png";
+import cross from "../../assets/cross.png";
+import { useEffect, useState } from "react";
 
-export default function EntryInfoModal() {
+import * as portfolioAPI from "../../APIs/portfolioAPI.js";
+import {
+    calculateHoldings,
+    calculatePNL,
+    calculatePNLPercentage,
+} from "../../utils/calculationsUtils.js";
+import { formatDate } from "../../utils/dateUtils.js";
+
+export default function EntryInfoModal({ entryId, onClose }) {
+    const [entryDetails, setEnetryDetails] = useState({});
+
+    console.log(entryDetails);
+
+    useEffect(() => {
+        portfolioAPI.getOne(entryId).then((result) => setEnetryDetails(result));
+    }, [entryId]);
+
     return (
         <div className="app-modal">
             <div className="app-modal-content d-flex-column gap-4 add-modal-position">
                 <div className="modal-title">
                     <span>Transaction Details</span>
-                    <img src={cross} width="18" height="18" />
+                    <button onClick={onClose}>
+                        <img src={cross} width="18" height="18" />
+                    </button>
                 </div>
                 <div className="modal-content">
                     <div className="d-flex-between-content px-3">
@@ -16,36 +35,42 @@ export default function EntryInfoModal() {
                     <div className="divider my-3"></div>
                     <div className="d-flex-between-content px-3">
                         <span className="secondary-text-color">Date</span>
-                        <span>Dec 24, 2022, 11:20PM</span>
+                        <span>{formatDate(entryDetails.date)}</span>
                     </div>
                     <div className="divider my-3"></div>
                     <div className="d-flex-between-content px-3">
                         <span className="secondary-text-color">
                             Price per coin
                         </span>
-                        <span>$8.9970</span>
+                        <span>${entryDetails.price}</span>
                     </div>
                     <div className="divider my-3"></div>
                     <div className="d-flex-between-content px-3">
                         <span className="secondary-text-color">Quantity</span>
-                        <span>10ATOM</span>
+                        <span>{`${entryDetails.quantity} ${entryDetails.symbol}`}</span>
                     </div>
                     <div className="divider my-3"></div>
                     <div className="d-flex-between-content px-3">
-                        <span className="secondary-text-color">Fees</span>
-                        <span>$0.09</span>
+                        <span className="secondary-text-color">Fee</span>
+                        <span>{entryDetails.fee}%</span>
                     </div>
                     <div className="divider my-3"></div>
                     <div className="d-flex-between-content px-3">
                         <span className="secondary-text-color">
                             Total Spent
                         </span>
-                        <span>$90.06</span>
+                        <span>
+                            $
+                            {calculateHoldings(
+                                entryDetails.price,
+                                entryDetails.quantity
+                            )}
+                        </span>
                     </div>
                     <div className="divider my-3"></div>
                     <div className="d-flex-column px-3">
-                        <span className="secondary-text-color">Notes</span>
-                        <span>-</span>
+                        <span className="secondary-text-color">Note</span>
+                        <span>{entryDetails.note}</span>
                     </div>
                     <div className="divider my-4"></div>
                 </div>
