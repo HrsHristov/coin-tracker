76891de5ee79da93ee95455ce0319ba4f982e0aa
Hristo Hristov <hrshristov@gmail.com>
2023-11-19 17:25:19 +0200

    Add proper info

--- client/src/components/PortfolioListRow/PortfolioListRow.jsx
+++ client/src/components/PortfolioListRow/PortfolioListRow.jsx
@@ -1,81 +1,117 @@
+import plus from "../../assets/plus.png";
+import binIcon from "../../assets/bin.png";
+import infoIcon from "../../assets/info.png";
+
 import { useState, useEffect } from "react";
 import { formatNumber, formatPrice } from "../../utils/formatUtils.js";
 import {
     calculateHoldings,
     calculatePNL,
     calculatePNLPercentage,
-} from "../../utils/calculations.js";
+} from "../../utils/calculationsUtils.js";
+import * as coinsAPI from "../../APIs/coinsAPI.js";
 
-import * as portfolioAPI from "../../APIs/portfolioAPI.js";
+export default function PortfolioListRow({
+    _id,
+    uuid,
+    name,
+    symbol,
+    iconUrl,
+    price,
+    quantity,
+    onDeleteClick,
+    onInfoClick,
+}) {
+    const [coinInfo, setCoinInfo] = useState({});
 
-import plus from "../../assets/plus.png";
-import binIcon from "../../assets/bin.png";
-import infoIcon from "../../assets/info.png";
-
-export default function PortfolioListRow({ uuid, price, quantity }) {
-    const [coinInfo, setCoinInfo] = useState([]);
+    console.log(coinInfo);
 
     useEffect(() => {
-        portfolioAPI
+        coinsAPI
             .getCoinInfo(uuid)
             .then((result) => setCoinInfo(result))
             .catch((err) => console.log(err));
-    }, []);
+    }, [uuid]);
+
+    const infoClickHandler = () => {
+        onInfoClick(_id);
+    };
+
+    const deleteClickHandler = () => {
+        onDeleteClick(_id);
+    };
 
     return (
         <>
-            {coinInfo.map((info) => (
-                <div key={uuid}>
-                    <div className="divider"></div>
-                    <div className="coin-row">
-                        <div className="d-flex gap-2 coin-icon-conteiner">
-                            <div className="coin-icon">
-                                <img className="img" src={info.iconUrl} />
-                            </div>
-                            <div className="d-flex-center-content gap-2">
-                                <span>{info.name}</span>
-                                <span>{info.symbol}</span>
-                            </div>
-                        </div>
-                        <div>${formatPrice(info.price)}</div>
-                        {info.change > 0 ? (
-                            <div className="up">+{info.change}%</div>
-                        ) : (
-                            <div className="down">{info.change}%</div>
-                        )}
-                        <div className="d-flex-column gap-1 text-end">
-                            <div>${calculateHoldings(price, quantity)}</div>
-                            <div>
-                                {quantity} {info.symbol}
-                            </div>
-                        </div>
-                        <div>${formatPrice(price.toString())}</div>
-                        <div className="d-flex-column gap-1 text-end">
-                            <div>
-                                ${calculatePNL(info.price, price, quantity)}
-                            </div>
-
-                            {info.price > price ? (
-                                <div className="up">
-                                    {calculatePNLPercentage(info.price, price)}%
-                                </div>
-                            ) : (
-                                <div className="down">
-                                    {calculatePNLPercentage(info.price, price)}%
-                                </div>
-                            )}
-                        </div>
-                        <div className="d-flex-end-content gap-2">
-                            <button type="submit" className="button-action">
-                                <img src={infoIcon} width="16" height="16" />
-                            </button>
-                            <button type="submit" className="button-action">
-                                <img src={binIcon} width="18" height="18" />
-                            </button>
-                        </div>
+            <div className="divider"></div>
+            <div className="coin-row">
+                <div className="d-flex gap-2 coin-icon-conteiner">
+                    <div className="coin-icon">
+                        <img className="img" src={iconUrl} />
+                    </div>
+                    <div className="d-flex-center-content gap-2">
+                        <span>{name}</span>
+                        <span>{symbol}</span>
                     </div>
                 </div>
-            ))}
+                <div>${formatPrice(coinInfo.data.coin.price.toString())}</div>
+                {coinInfo.data.coin.change > 0 ? (
+                    <div className="up">+{coinInfo.data.coin.change}%</div>
+                ) : (
+                    <div className="down">{coinInfo.data.coin.change}%</div>
+                )}
+                <div className="d-flex-column gap-1 text-end">
+                    <div>${calculateHoldings(price, quantity)}</div>
+                    <div>
+                        {quantity} {symbol}
+                    </div>
+                </div>
+                <div>${formatPrice(price.toString())}</div>
+                <div className="d-flex-column gap-1 text-end">
+                    <div>
+                        $
+                        {calculatePNL(
+                            coinInfo.data.coin.price,
+                            price,
+                            quantity
+                        )}
+                    </div>
+
+                    {coinInfo.data.coin.price > price ? (
+                        <div className="up">
+                            {calculatePNLPercentage(
+                                coinInfo.data.coin.price,
+                                price
+                            )}
+                            %
+                        </div>
+                    ) : (
+                        <div className="down">
+                            {calculatePNLPercentage(
+                                coinInfo.data.coin.price,
+                                price
+                            )}
+                            %
+                        </div>
+                    )}
+                </div>
+                <div className="d-flex-end-content gap-2">
+                    <button
+                        type="submit"
+                        className="button-action"
+                        onClick={infoClickHandler}
+                    >
+                        <img src={infoIcon} width="16" height="16" />
+                    </button>
+                    <button
+                        type="submit"
+                        className="button-action"
+                        onClick={deleteClickHandler}
+                    >
+                        <img src={binIcon} width="18" height="18" />
+                    </button>
+                </div>
+            </div>
         </>
     );
 }
