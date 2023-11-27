36410cab079b9bc8b35fed76aba8f0185b4b6fd2
Hristo Hristov <hrshristov@gmail.com>
2023-11-27 17:29:53 +0200

    Get data dynamicaly for CoinDetails

--- client/src/components/CoinDetails/CoinDetails.jsx
+++ client/src/components/CoinDetails/CoinDetails.jsx
@@ -1,36 +1,72 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import * as coinsService from "../../servies/coinsService";
+import { formatNumber, formatPrice } from "../../utils/formatUtils";
+
 import ListWrapper from "../ListWrapper/ListWrapper";
 
 export default function CoinDetails() {
+    const [coinInfo, setCoinInfo] = useState(null);
+    const { uuid } = useParams();
+
+    useEffect(() => {
+        coinsService.getCoinInfo(uuid).then((result) => setCoinInfo(result));
+    }, []);
+
+    if (!coinInfo) {
+        return <div>loading....</div>;
+    }
+
+    const name = coinInfo.data.coin.name;
+    const price = formatPrice(coinInfo.data.coin.price);
+    const description = coinInfo.data.coin.description;
+    const iconUrl = coinInfo.data.coin.iconUrl;
+
+    const volume = formatNumber(coinInfo.data.coin["24hVolume"]);
+    const change = coinInfo.data.coin.change;
+
+    const ath = formatNumber(coinInfo.data.coin.allTimeHigh.price);
+    const athDate = new Date(
+        coinInfo.data.coin.allTimeHigh.timestamp * 1000
+    ).toLocaleString();
+
+    const marketCap = formatNumber(coinInfo.data.coin.marketCap);
+    const fullyDilutedMarketCap = formatNumber(
+        coinInfo.data.coin.fullyDilutedMarketCap
+    );
+
     return (
         <ListWrapper>
+            {/* Chart section */}
+            <div>
+                <h1>CHART HERE</h1>
+            </div>
+
             {/* "Header" section with Logo, Name, Price */}
             <div>
                 {/* bitcoin logo infront of the name */}
-                <h2>Bitcoin</h2>
-                <p>$36,256.25</p>
-                <p>
-                    Bitcoin is a digital currency with a finite supply, allowing
-                    users to send/receive money without a central
-                    bank/government, often nicknamed "Digital Gold"
-                </p>
-            </div>
+                <div className="coin-icon">
+                    <img className="img" src={iconUrl} />
+                    <h2>{name}</h2>
+                </div>
 
-            {/* Chart section */}
-            <div>
-                <p>PUT chart here</p>
+                <p>${price}</p>
+                <p>{description}</p>
             </div>
 
             {/* Section with  with recent price movements*/}
             <div>
                 <div>
                     <h5>Volume 24h</h5>
-                    <p>29k</p>
-                    <p className="up">+1.14%</p>
-                </div>
-
-                <div>
-                    <h5>Volume 24h</h5>
-                    <p>29k</p>
+                    <p>{volume}</p>
+                    <p className="up">
+                        {change > 0 ? (
+                            <div className="up">+{change}%</div>
+                        ) : (
+                            <div className="down">{change}%</div>
+                        )}
+                    </p>
                 </div>
             </div>
 
@@ -38,12 +74,16 @@ export default function CoinDetails() {
             <div>
                 <div>
                     <h5>All Time High</h5>
-                    <p>68.763,25</p>
+                    <p>{ath}</p>
+                    <p>On Date: {athDate}</p>
                 </div>
 
                 <div>
-                    <p>marketCap</p>
-                    <p>29k</p>
+                    <p>MarketCap</p>
+                    <p>{marketCap}</p>
+
+                    <p>FullyDilutedMarketCap</p>
+                    <p>{fullyDilutedMarketCap}</p>
                 </div>
             </div>
         </ListWrapper>
