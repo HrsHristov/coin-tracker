3fb8ea7692529ad4ef4dc256167a02351ebc527e
Hristo Hristov <hrshristov@gmail.com>
2023-11-28 21:13:44 +0200

    Magi register controlled

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,39 +1,60 @@
+import { useContext } from "react";
+import AuthContext from "../../Contexts/authContext";
+
+import useForm from "../../hooks/useForm";
 import Button from "../Button/Button";
 import ModalWrapper from "../Modals/ModalWrapper";
+import Input from "../Input/Input";
 
 import reactImg from "../../assets/react-core-concepts.png";
-import Label from "../Label/Label";
-import Input from "../Input/Input";
-import CreateEntryModal from "../Modals/CreateEntryModal/CreateEntryModal";
+
+const RegisterFormKeys = {
+    Email: "email",
+    Password: "password",
+    ConfirmPassword: "confirm-password",
+};
 
 export default function Register() {
+    const { registerSubmitHandler } = useContext(AuthContext);
+    const { values, onChange, onSubmit } = useForm(registerSubmitHandler, {
+        [RegisterFormKeys.Email]: "",
+        [RegisterFormKeys.Password]: "",
+        [RegisterFormKeys.ConfirmPassword]: "",
+    });
+
     return (
         <ModalWrapper title="Register">
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="header_logo">
                     <img src={reactImg} alt="CoinTrack's logo" />
                 </div>
                 <Input
                     labelName="Email"
                     type="email"
-                    name="email"
+                    name={RegisterFormKeys.Email}
                     placeholder="maria@email.com"
                     id="email"
+                    value={values[RegisterFormKeys.Email]}
+                    onChange={onChange}
                 />
 
                 <Input
                     labelName="Password"
                     type="password"
-                    name="password"
+                    name={RegisterFormKeys.Password}
                     placeholder="Enter password"
                     id="register-password"
+                    value={values[RegisterFormKeys.Password]}
+                    onChange={onChange}
                 />
                 <Input
                     labelName="Confirm Password"
-                    type="confirm-password"
-                    name="confirm-password"
+                    type="password"
+                    name={RegisterFormKeys.ConfirmPassword}
                     placeholder="Enter password again"
                     id="register-password"
+                    value={values[RegisterFormKeys.ConfirmPassword]}
+                    onChange={onChange}
                 />
 
                 <Button primary>Register</Button>
