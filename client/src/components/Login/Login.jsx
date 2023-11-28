0ec5d2ef450093e8accedd3611a881812707bfdc
Hristo Hristov <hrshristov@gmail.com>
2023-11-28 21:13:36 +0200

    Make login controlled

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,19 +1,36 @@
+import { useContext } from "react";
+
 import Button from "../Button/Button";
 import Card from "../Card/Card";
 import Input from "../Input/Input";
+import useForm from "../../hooks/useForm";
+import AuthContext from "../../Contexts/authContext";
+
+const LoginFormKeys = {
+    Email: "email",
+    Password: "password",
+};
 
 export default function Login() {
+    const { loginSubmitHandler } = useContext(AuthContext);
+    const { values, onChange, onSubmit } = useForm(loginSubmitHandler, {
+        [LoginFormKeys.Email]: "",
+        [LoginFormKeys.Passwordassword]: "",
+    });
+
     return (
         <div className="container container--sm my-5">
             <Card>
-                <form>
+                <form onSubmit={onSubmit}>
                     <div className="form-group">
                         <Input
                             labelName="Email"
                             type="email"
-                            name="email"
+                            name={LoginFormKeys.Email}
                             placeholder="example@gmail.com"
                             id="email"
+                            value={values[LoginFormKeys.Email]}
+                            onChange={onChange}
                         />
                     </div>
 
@@ -21,9 +38,11 @@ export default function Login() {
                         <Input
                             labelName="Password"
                             type="password"
-                            name="password"
+                            name={LoginFormKeys.Password}
                             placeholder="Enter password"
                             id="password"
+                            value={values[LoginFormKeys.Password]}
+                            onChange={onChange}
                         />
                     </div>
 
