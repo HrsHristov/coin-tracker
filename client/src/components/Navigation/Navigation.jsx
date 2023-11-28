3131c9652f50214c50c3cab517b4041e965070fb
Hristo Hristov <hrshristov@gmail.com>
2023-11-28 21:14:13 +0200

    Add context to navigation

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -1,18 +1,46 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+
+import AuthContext from "../../Contexts/authContext";
+
+import Button from "../Button/Button";
 import "./Navigation.css";
 
 // TODO - Add structure
 // Use router name
 
 export default function Navigation() {
+    const { isAuthenticated, email } = useContext(AuthContext);
     return (
-        <nav className="header-nav">
-            <Link to="/coins" className="header-nav__link">
-                Coins
-            </Link>
-            <Link to="/portfolio" className="header-nav__link">
-                Portfolio
-            </Link>
-        </nav>
+        <>
+            <nav className="header-nav">
+                <Link to="/coins" className="header-nav__link">
+                    Coins
+                </Link>
+                <Link to="/portfolio" className="header-nav__link">
+                    Portfolio
+                </Link>
+            </nav>
+
+            {isAuthenticated && (
+                <div className="header_actions">
+                    <Link to="/login">
+                        <Button primary>Login</Button>
+                    </Link>
+                    <Link to="/register">
+                        <Button secondary>Sign Up</Button>
+                    </Link>
+                </div>
+            )}
+
+            {!isAuthenticated && (
+                <div className="header_actions">
+                    <span>{email} | </span>
+                    <Link to="/logout">
+                        <Button primary>logout</Button>
+                    </Link>
+                </div>
+            )}
+        </>
     );
 }
