b30e295fd826cb848e617c2dc3a60fa77ce2a59f
Hristo Hristov <hrshristov@gmail.com>
2023-11-29 13:30:29 +0200

    Change ModalWrapper with Card

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -3,10 +3,10 @@ import AuthContext from "../../Contexts/authContext";
 
 import useForm from "../../hooks/useForm";
 import Button from "../Button/Button";
-import ModalWrapper from "../Modals/ModalWrapper";
 import Input from "../Input/Input";
 
 import reactImg from "../../assets/react-core-concepts.png";
+import Card from "../Card/Card";
 
 const RegisterFormKeys = {
     Email: "email",
@@ -23,42 +23,44 @@ export default function Register() {
     });
 
     return (
-        <ModalWrapper title="Register">
-            <form onSubmit={onSubmit}>
-                <div className="header_logo">
-                    <img src={reactImg} alt="CoinTrack's logo" />
-                </div>
-                <Input
-                    labelName="Email"
-                    type="email"
-                    name={RegisterFormKeys.Email}
-                    placeholder="maria@email.com"
-                    id="email"
-                    value={values[RegisterFormKeys.Email]}
-                    onChange={onChange}
-                />
+        <div className="container container--sm my-5">
+            <Card>
+                <form onSubmit={onSubmit}>
+                    <div className="header_logo">
+                        <img src={reactImg} alt="CoinTrack's logo" />
+                    </div>
+                    <Input
+                        labelName="Email"
+                        type="email"
+                        name={RegisterFormKeys.Email}
+                        placeholder="maria@email.com"
+                        id="email"
+                        value={values[RegisterFormKeys.Email]}
+                        onChange={onChange}
+                    />
 
-                <Input
-                    labelName="Password"
-                    type="password"
-                    name={RegisterFormKeys.Password}
-                    placeholder="Enter password"
-                    id="register-password"
-                    value={values[RegisterFormKeys.Password]}
-                    onChange={onChange}
-                />
-                <Input
-                    labelName="Confirm Password"
-                    type="password"
-                    name={RegisterFormKeys.ConfirmPassword}
-                    placeholder="Enter password again"
-                    id="register-password"
-                    value={values[RegisterFormKeys.ConfirmPassword]}
-                    onChange={onChange}
-                />
+                    <Input
+                        labelName="Password"
+                        type="password"
+                        name={RegisterFormKeys.Password}
+                        placeholder="Enter password"
+                        id="register-password"
+                        value={values[RegisterFormKeys.Password]}
+                        onChange={onChange}
+                    />
+                    <Input
+                        labelName="Confirm Password"
+                        type="password"
+                        name={RegisterFormKeys.ConfirmPassword}
+                        placeholder="Enter password again"
+                        id="register-password"
+                        value={values[RegisterFormKeys.ConfirmPassword]}
+                        onChange={onChange}
+                    />
 
-                <Button primary>Register</Button>
-            </form>
-        </ModalWrapper>
+                    <Button primary>Register</Button>
+                </form>
+            </Card>
+        </div>
     );
 }
