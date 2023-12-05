b95bcd506465bca4c3eb3fd5e62bb515beb53ca7
Hristo Hristov <hrshristov@gmail.com>
2023-12-05 11:19:11 +0200

    rename data

--- client/src/services/portfolioService.js
+++ client/src/services/portfolioService.js
@@ -1,7 +1,6 @@
 import * as request from "../lib/request";
 
 const baseUrl = "http://localhost:3030/data/portfolio";
-// const baseUrl = "http://localhost:3030/jsonstore/portfolio";
 
 export const getAllEntries = async (userId) => {
     const query = new URLSearchParams({
@@ -21,8 +20,8 @@ export const getOne = async (entryId) => {
     return result;
 };
 
-export const create = async (gameData) => {
-    const result = await request.post(baseUrl, gameData);
+export const create = async (data) => {
+    const result = await request.post(baseUrl, data);
 
     console.log(result);
 
