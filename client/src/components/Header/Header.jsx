6840cc4aadb6278113170edbd405b9f2d57d66c9
Hristo Hristov <hrshristov@gmail.com>
2023-12-05 23:39:34 +0200

    Add styles

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,15 +1,21 @@
+import { Link } from "react-router-dom";
 import reactImg from "../../assets/logo.png";
 import Navigation from "../Navigation/Navigation";
 
 import "./Header.css";
+import Path from "../../paths";
 
 const Header = () => {
     return (
         <header className="header">
             <div className="container">
                 <div className="header__logo">
-                    <img src={reactImg} alt="CoinTrack's logo" />
-                    <span>CoinTrack</span>
+                    <Link to={Path.Coins}>
+                        <img src={reactImg} alt="CoinTrack's logo" />
+                    </Link>
+                    <Link className="home" to={Path.Coins}>
+                        CoinTrack
+                    </Link>
                 </div>
 
                 <Navigation />
