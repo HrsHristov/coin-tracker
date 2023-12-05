79ecd2ddd7203e48b973f70d0b751ccda374acdd
Hristo Hristov <hrshristov@gmail.com>
2023-12-05 11:19:53 +0200

    adjust select styling

--- client/src/components/Select/Select.jsx
+++ client/src/components/Select/Select.jsx
@@ -1,21 +1,25 @@
+import Label from "../Label/Label";
 import "./Select.css";
 
-const Select = ({ data, name, id, onChange }) => {
+const Select = ({ data, name, id, type, labelName, onChange }) => {
     return (
-        <select
-            name={name}
-            id={id}
-            onChange={onChange}
-            className="form-group__select"
-        >
-            <option value="">Select type...</option>
-            {data.map((coin) => (
-                <option
-                    key={coin.name}
-                    value={coin.uuid}
-                >{`${coin.name} (${coin.symbol})`}</option>
-            ))}
-        </select>
+        <div className="form-group">
+            <Label htmlFor={type}>{labelName}:</Label>
+            <select
+                name={name}
+                id={id}
+                onChange={onChange}
+                className="form-group__select"
+            >
+                <option value="">Select type...</option>
+                {data.map((coin) => (
+                    <option
+                        key={coin.name}
+                        value={coin.uuid}
+                    >{`${coin.name} (${coin.symbol})`}</option>
+                ))}
+            </select>
+        </div>
     );
 };
 
